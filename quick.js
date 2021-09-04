391ac2fc0d666fcb86b6ca5b980e92718590c0f1
calebpenning <calebpenning@mail.com>
2021-09-04 11:49:51 -0700

    finished

--- quick.js
+++ quick.js
@@ -3,12 +3,38 @@ pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-const pivot = () => {}
+const pivot = (arr=[], start=0, end=arr.length - 1) => {
+    let pivotVal = arr[start]
+    let swapIdx = start
+
+    for (let i = start + 1; i <= end; i++) {
+        if (pivotVal > arr[i]) {
+            swapIdx++
+            swap(swapIdx, i, arr)
+        }
+    }
+    swap(start, swapIdx, arr)
+    return swapIdx
+}
+
+const swap = (idx1, idx2, arr) => {
+    let temp = arr[idx1]
+    arr[idx1] = arr[idx2]
+    arr[idx2] = temp
+}
+
 
 /*
 quickSort accepts an array, left index, and right index
 */
 
-function quickSort() {}
+const quickSort = (arr=[], start=0, end=arr.length - 1) => {
+    if (start < end) {
+        let pivotIdx = pivot(arr, start, end)
+        quickSort(arr, start, pivotIdx - 1)
+        quickSort(arr, pivotIdx + 1, end)
+    }
+    return arr
+}
 
-module.exports = quickSort;
+module.exports = {pivot, quickSort};
--- radix.js
+++ radix.js
@@ -1,3 +1,32 @@
-function radixSort() {}
+const getDigit = (num, i) => (
+    Math.floor(Math.abs(num) / Math.pow(10, i)) % 10
+)
 
-module.exports = radixSort;
+const digitCount = num => {
+    if (num === 0) return 1
+    return Math.floor(Math.log10(Math.abs(num))) + 1
+}
+
+const mostDigits = nums => {
+    let maxDigits = 0
+    for (let i = 0; i < nums.length; i++) {
+        maxDigits = Math.max(maxDigits, digitCount(nums[i]))
+    }
+    return maxDigits
+}
+
+const radixSort = nums => {
+    let maxDigitCount = mostDigits(nums)
+    for (let k = 0; k < maxDigitCount; k++) {
+        let digitBuckets = Array.from({length: 10}, () => [])
+        for (let i = 0; i < nums.length; i++) {
+            let num = nums[i]
+            let digit = getDigit(num, k)
+            digitBuckets[digit].push(num)
+        }
+        nums = [].concat(...digitBuckets)
+    }
+    return nums
+}
+
+module.exports = { getDigit, digitCount, mostDigits, radixSort};
